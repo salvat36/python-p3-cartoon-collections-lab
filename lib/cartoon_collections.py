6e4f1f4b572f0b6440cbb251442fd4d97d66694b
Ryan <salvat36@gmail.com>
2023-05-26 23:19:38 -0500

    Passing Labs

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,19 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    for number, dwarf in enumerate(dwarves):
+        print(f"{number + 1}. {dwarf}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteer_calls):
+    calls = [element.title() + "!" for element in planeteer_calls]
+    return calls
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(items):
+    for item in items: 
+        if len(item) > 4:
+            return True
+    else:
+        return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(items):
+    for item in items:
+        if item == "cheddar" or item == "gouda" or item == "camembert":
+            return item
